58601ec9218304564236c614d19888e49f41580a
kwrona05 <kwrona05@gmail.com>
2024-09-04 14:09:42 +0200

    create Vite

---
+++ Page/components/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+        <div className="container">
+  <header>
+    <img src="" alt="logo" />
+    <Link to="About">O nas</Link>
+    <Link to="Calendar">Kalendarz</Link>
+    <Link to="News">Zapowiedzi</Link>
+    <Link to="Messages">Komunikaty</Link>
+    <Link to="Events">Wydarzenia</Link>
+  </header>
+    )
+}
---
+++ Page/src/App.jsx
@@ -0,0 +1,10 @@
+import { Router, Routes, Route } from 'react-router-dom'
+function App() {
+<div className="container">
+  <header>
+    <img src="" alt="logo" />
+  </header>
+</div>
+}
+
+export default App
